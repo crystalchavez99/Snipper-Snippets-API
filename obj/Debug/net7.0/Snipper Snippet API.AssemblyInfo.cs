9337337313eb95f1d7eb09a2cc4c6704c0611528
crystalchavez99 <chavezcrystal1999@yahoo.com>
2024-01-18 10:22:49 -0800

    added seed data

--- obj/Debug/net7.0/Snipper Snippet API.AssemblyInfo.cs
+++ obj/Debug/net7.0/Snipper Snippet API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Snipper Snippet API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9ecd640646b8ce1bc03e2477ee1e343b642350a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Snipper Snippet API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Snipper Snippet API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
