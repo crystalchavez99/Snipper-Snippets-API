328a4d0a2290583c9f81045f058a8d7ccfdc3414
crystalchavez99 <chavezcrystal1999@yahoo.com>
2024-01-18 10:46:49 -0800

    Create controller for web api

--- obj/Debug/net7.0/Snipper Snippet API.AssemblyInfo.cs
+++ obj/Debug/net7.0/Snipper Snippet API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Snipper Snippet API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9ecd640646b8ce1bc03e2477ee1e343b642350a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9337337313eb95f1d7eb09a2cc4c6704c0611528")]
 [assembly: System.Reflection.AssemblyProductAttribute("Snipper Snippet API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Snipper Snippet API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
